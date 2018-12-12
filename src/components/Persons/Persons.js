f6bd119f59311609a12fc6128727e0dbd86ba357
Agustin Pane <agustin.pane@gmail.com>
2018-12-12 09:27:43 -0300

    Refactor in components

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,32 @@
+import React, {Component } from 'react';
+import Person from './Person/Person';
+
+class Persons extends Component{
+
+    constructor(props){
+        super(props);
+        console.log("[Person.js] Inside Constructor", props)
+    }
+
+    componentWillMount(){
+        console.log("[Person.js] Inside componentWillMount");
+    }
+
+    componentDidMount(){
+        console.log('[Person.js] Inside componentDidMount');
+    }
+
+    render(){
+        console.log('[Person.js] Inside Render');
+        return this.props.persons.map((person,index) =>{
+            return <Person
+                click={ () => this.props.clicked(index) }
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={( event ) => this.props.changed(event, person.id) }/>
+        });
+    }
+}
+
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
-
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+import classes from './App.css';
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        console.log("[App.js] Inside Constructor", props)
+    }
+
+    componentWillMount(){
+        console.log("[App.js] Inside componentWillMount");
+    }
+
+    componentDidMount(){
+        console.log('[App.js] Inside componentDidMount');
+    }
 
     state = {
         persons: [
@@ -14,7 +26,7 @@ class App extends Component {
     };
 
     switchNameHandler = (newName) => {
-      //console.log('Was clicked');
+
         this.setState({
             persons: [
             { name: newName, age: 28 },
@@ -45,6 +57,8 @@ class App extends Component {
     };
 
     togglePersonsHandler = () => {
+        console.log("Toggled persons");
+        console.log("Show persons: ", this.state.showPersons);
         const doesShow = this.state.showPersons;
         this.setState({
             showPersons: !doesShow
@@ -64,61 +78,29 @@ class App extends Component {
     };
 
   render() {
-
-      const style = {
-          backgroundColor: 'green',
-          color: 'white',
-          font: 'inherit',
-          border: '1px solid blue',
-          padding: '8px',
-          cursor: 'pointer',
-          ':hover': {
-              backgroundColor: 'lightgreen',
-              color: 'black'
-          }
-      };
-
+      console.log("[App.js] Inside render");
       let persons;
 
+      console.log("App classes ", classes);
       if( this.state.showPersons ) {
           persons = (
               <div>
-                  {
-                      this.state.persons.map( (person, index) => {
-                          return <Person
-                              click={ () => this.deletePersonHandler(index) }
-                              name={person.name}
-                              age={person.age}
-                              key={ person.id}
-                          changed={ (event) => this.nameChangedHandler(event, person.id) }/>
-                      })
-                  }
+                  <Persons
+                      persons={this.state.persons}
+                      clicked={this.deletePersonHandler}
+                      changed={this.nameChangedHandler} />
               </div>
           );
-
-          style.backgroundColor = 'red';
-      }
-
-      let classes = [];
-
-      if(this.state.persons.length <= 2){
-          classes.push('red'); // classes = ['red']
-      }
-      if(this.state.persons.length <= 1){
-          classes.push('bold'); // classes = ['red', 'bold']
       }
 
       return(
-          <StyleRoot>
-              <div className="App">
-                  <h1>Hi, I'm a React App</h1>
-                  <p className={classes.join(' ')}>This is really working!</p>
-                  <button
-                      style = { style }
-                      onClick={ this.togglePersonsHandler }>Show persons</button>
-                  { persons }
-              </div>
-          </StyleRoot>
+          <div>
+              <Cockpit
+                  showPersons={this.state.showPersons}
+                  persons={this.state.persons}
+                  clicked={this.togglePersonsHandler} />
+              { persons }
+          </div>
       );
   }
 }
